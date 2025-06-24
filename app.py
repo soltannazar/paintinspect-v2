d4cba05061a250512c6f681a65806e2b166bb803
soltannazar <130620025+soltannazar@users.noreply.github.com>
2025-06-25 01:45:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import numpy as np
 import base64
 import io
 from PIL import Image
+import os
 
 app = Flask(__name__)
 
@@ -81,4 +82,5 @@ def analyze():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
